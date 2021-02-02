b048d0f49aff119c294e7e249411b99e5903d3c5
J. Gregory Wright <daecabhir@gmail.com>
2021-02-01 21:08:48 -0500

    WIP - Working integration test for get/set values
    Not really advancing the whole cloud config aspect per se, but just
    making sure I grok how Spring Vault work, so that I can initialize
    values before the integration tests.

--- cloud-config-server/src/integration-test/java/com/daecabhir/cloudconfigserver/CloudConfigServerApplicationTests.java
+++ cloud-config-server/src/integration-test/java/com/daecabhir/cloudconfigserver/CloudConfigServerApplicationTests.java
@@ -1,54 +1,76 @@
 package com.daecabhir.cloudconfigserver;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import lombok.extern.jackson.Jacksonized;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.vault.core.VaultVersionedKeyValueOperations;
 import org.springframework.vault.support.VaultResponseSupport;
 import org.springframework.vault.support.Versioned;
 
+import java.util.List;
 
+
+@Slf4j
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 // Have to list them all because it doesn't want to pick them up from src/main/resources/application.yml
 @ActiveProfiles({"test", "native", "vault"})
 public class CloudConfigServerApplicationTests {
 
-	private static final String CLIENT_APP_KEY = "secret/client-app/cloud-config-server";
+	private static final String CLIENT_APP_PATH = "/cloud-config-example/client-app";
 
 	@Autowired
 	private VaultVersionedKeyValueOperations kvOperations;
+
+	@Value("${test.vault.secretsPath}")
+	private String secretsPath;
+
+	private String dataPath;
+
+	private String defaultSecretsPath;
 
 	private ClientAppSecrets secrets;
 
 	@BeforeAll
 	private void beforeAll() {
-		secrets = new ClientAppSecrets();
-		secrets.setUsername("myuser");
-		secrets.setPassword("mypassword");
+		secrets = ClientAppSecrets.builder()
+				.username("myuser")
+				.password("mypassword")
+				.build();
 
-		kvOperations.put(CLIENT_APP_KEY, secrets);
+		Versioned.Metadata metadata = kvOperations.put(CLIENT_APP_PATH, Versioned.create(secrets));
+		assertThat(metadata.isDestroyed(), is(false));
+		assertThat(metadata.isDeleted(), is(false));
+		assertThat(metadata.getCreatedAt(), is(notNullValue()));
 	}
 
 	@Test
 	public void secretsStored () {
-		Versioned<ClientAppSecrets> response = kvOperations.get(CLIENT_APP_KEY, ClientAppSecrets.class);
+		List<String> keys = kvOperations.list("/");
+		log.info("Root Keys: {}", keys);
+		keys = kvOperations.list(CLIENT_APP_PATH);
+		log.info("Client Keys: {}", keys);
+
+		Versioned<ClientAppSecrets> response = kvOperations.get(CLIENT_APP_PATH, ClientAppSecrets.class);
+		assertThat(response.hasData(), is(true));
 		ClientAppSecrets storedSecrets = response.getData();
 		assertThat(storedSecrets, is(equalTo(secrets)));
 	}
 
+	@Jacksonized
+	@Builder
 	@EqualsAndHashCode
-	public class ClientAppSecrets {
+	public static class ClientAppSecrets {
 		@Getter
 		@Setter
 		private String username;
--- cloud-config-server/src/integration-test/java/com/daecabhir/cloudconfigserver/test/VaultTestConfig.java
+++ cloud-config-server/src/integration-test/java/com/daecabhir/cloudconfigserver/test/VaultTestConfig.java
@@ -1,11 +1,14 @@
 package com.daecabhir.cloudconfigserver.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.vault.authentication.ClientAuthentication;
 import org.springframework.vault.authentication.TokenAuthentication;
 import org.springframework.vault.client.VaultEndpoint;
 import org.springframework.vault.config.AbstractVaultConfiguration;
+import org.springframework.vault.core.*;
 
 @Configuration
 public class VaultTestConfig extends AbstractVaultConfiguration {
@@ -13,15 +16,18 @@ public class VaultTestConfig extends AbstractVaultConfiguration {
   final String vaultHost;
   final int vaultPort;
   final String authToken;
+  final String secretsPath;
 
   public VaultTestConfig(
     @Value("${test.vault.host}") final String vaultHost,
     @Value("${test.vault.port}") final int vaultPort,
-    @Value("${test.vault.token}") final String authToken) {
+    @Value("${test.vault.token}") final String authToken,
+    @Value("${test.vault.secretsPath}") final String secretsPath) {
 
     this.vaultHost = vaultHost;
     this.vaultPort = vaultPort;
     this.authToken = authToken;
+    this.secretsPath = secretsPath;
   }
 
   @Override
@@ -36,5 +42,10 @@ public class VaultTestConfig extends AbstractVaultConfiguration {
   @Override
   public ClientAuthentication clientAuthentication() {
     return new TokenAuthentication(authToken);
+  }
+
+  @Bean
+  public VaultVersionedKeyValueOperations versionedKeyValueOperations(@Autowired VaultOperations vaultOperations) {
+    return vaultOperations.opsForVersionedKeyValue(secretsPath);
   }
 }
